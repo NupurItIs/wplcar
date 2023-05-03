4c79eea5c23650e69d04e53861722272b1adcc0c
Nupur Chaudhari <105906058+NupurItIs@users.noreply.github.com>
2023-05-03 15:22:09 +0530

    1st

---
+++ car-go-wpl-main/firebase.js
@@ -0,0 +1,19 @@
+// import  initializeApp  from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-analytics.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB_ir9Z-orm6hAC5FM4NiTPP21hJySA85c",
+//   authDomain: "wpl-miniproject-5cee8.firebaseapp.com",
+//   projectId: "wpl-miniproject-5cee8",
+//   storageBucket: "wpl-miniproject-5cee8.appspot.com",
+//   messagingSenderId: "64870738232",
+//   appId: "1:64870738232:web:0a5e95de7ba5fc588779cd",
+//   measurementId: "G-D16E7ETNYQ",
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// const analytics = getAnalytics(app);
+// const auth = firebase.auth();
+// const db = firebase.firestore();
+
---
+++ car-go-wpl-main/index.js
@@ -0,0 +1,44 @@
+//   import { initializeApp } from "firebase/app";
+//   import { getAnalytics } from "firebase/analytics";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
